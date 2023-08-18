1eee47e988db8bd1e4acd9211c7430ab70d8dd74
Camilo Bacca <camiloandresbvbav@ufps.edu.co>
2023-08-18 08:03:42 -0500

    feat:Se agrego componente Perfil

--- frontend/src/app/perfil/perfil.component.ts
+++ frontend/src/app/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormBuilder,Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent {
+  formPerfil: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.formPerfil = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      correo: ['', Validators.required],
+      teléfono: ['', Validators.required],
+      foto: ['', Validators.required],
+    })
+  }
+  handleFormSubmit(form: FormGroup) {
+
+
+  }
 
 }
