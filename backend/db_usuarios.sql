7e8f87dd167b19d540dbf94523add232bcc4277f
Camilo Bacca <camiloandresbvbav@ufps.edu.co>
2023-08-18 08:37:14 -0500

    refactor: se cambio contraseña a password

--- backend/db_usuarios.sql
+++ backend/db_usuarios.sql
@@ -3,8 +3,9 @@ use db_usuarios;
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
-    apelllido varchar(50) not null,
-    correo VARCHAR(100) NOT NULL unique,
-    contraseña VARCHAR(255) NOT NULL
+    apellido varchar(50) not null,
+    correo VARCHAR(100) NOT NULL ,
+   password VARCHAR(255) NOT NULL
 );
-
+select *from usuarios;
+INSERT INTO usuarios (nombre, apellido, correo, password) VALUES ('Camilo', 'Bacca', 'ca@hot.com','camilo12' )
